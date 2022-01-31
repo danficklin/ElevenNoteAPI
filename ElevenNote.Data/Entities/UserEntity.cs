cf7f7be1ff772de89505730aa08ce8b115f38e06
Dan Ficklin <danficklin@fakemail.com>
2022-01-31 10:57:25 -0500

    Established user table

---
+++ ElevenNote.Data/Entities/UserEntity.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data.Entities
+{
+    public class UserEntity
+    {
+        [Key]
+        public int Id { get; set}
+        [Required]
+        public string Email { get; set}
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Forename { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public DateTime DateCreated { get; set; }
+    }
+}
